4740107649269ae87507e50d8ab58353ff9b7b0e
Dharmendra Ahirwar <dharmendra.ahirwar101@gmail.com>
2024-12-04 21:06:43 +0530

    completed go along exercise

--- src/main/java/com/example/Newsfeed.java
+++ src/main/java/com/example/Newsfeed.java
@@ -1,14 +1,22 @@
 package com.example;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Newsfeed {
+public class Newsfeed implements PropertyChangeListener {
 
     private List<String> statuses = new ArrayList<>();
 
     public void printStatuses() {
         statuses.forEach(System.out::println);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent event) {
+        // TODO Auto-generated method stub
+        statuses.add((String) event.getNewValue());
     }
 
 }
